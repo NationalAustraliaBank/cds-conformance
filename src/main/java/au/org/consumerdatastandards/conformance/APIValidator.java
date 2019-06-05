409e80256175c0697c18b9bbe033ae2fc734746b
fyang1024 <fyang1024@gmail.com>
2019-06-05 15:52:32 +1000

    Refactoring

---
+++ src/main/java/au/org/consumerdatastandards/conformance/APIValidator.java
@@ -0,0 +1,64 @@
+package au.org.consumerdatastandards.conformance;
+
+import au.org.consumerdatastandards.client.ApiClient;
+import au.org.consumerdatastandards.client.ApiException;
+import au.org.consumerdatastandards.client.api.BankingProductsAPI;
+import au.org.consumerdatastandards.client.model.*;
+import au.org.consumerdatastandards.codegen.ModelBuilder;
+import au.org.consumerdatastandards.codegen.generator.Options;
+import au.org.consumerdatastandards.conformance.util.ConformanceUtil;
+import au.org.consumerdatastandards.conformance.util.ModelConformanceConverter;
+import au.org.consumerdatastandards.support.ResponseCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class APIValidator {
+
+    private BankingProductsAPI bankingProductsAPI;
+
+    private ConformanceModel conformanceModel;
+
+    public APIValidator(String serverUrl) {
+        ModelBuilder modelBuilder = new ModelBuilder(new Options());
+        conformanceModel = ModelConformanceConverter.convert(modelBuilder.build());
+        ApiClient client = new ApiClient();
+        client.setBasePath(serverUrl);
+        bankingProductsAPI = new BankingProductsAPI(client);
+    }
+
+    public void validateAPI() {
+        validateBankingProductsAPI();
+    }
+
+    public void validateAPI(String apiName) {
+        if ("BankingProducts".equals(apiName)) {
+            validateBankingProductsAPI();
+        }
+    }
+
+    public void validateBankingProductsAPI() {
+        try {
+            ResponseBankingProductList allProducts = bankingProductsAPI.listProducts(ParamEffective.ALL, null, null, null, null, null);
+            List<ConformanceError> errors = new ArrayList<>();
+            Class<?> responseModel = conformanceModel.getResponse("listProducts", ResponseCode.OK).content();
+            ConformanceUtil.checkAgainstModel(allProducts, responseModel, errors);
+            for (BankingProduct product : allProducts.getData().getProducts()) {
+                validateProductListData(product);
+            }
+        } catch (ApiException e) {
+            System.out.println("API error: " + e.getMessage());
+        }
+    }
+
+    private void validateProductListData(BankingProduct product) {
+        try {
+            ResponseBankingProductById productDetail = bankingProductsAPI.getProductDetail(product.getProductId());
+            Class<?> responseModel = conformanceModel.getResponse("getProductDetail", ResponseCode.OK).content();
+            List<ConformanceError> errors = new ArrayList<>();
+            ConformanceUtil.checkAgainstModel(productDetail, responseModel, errors);
+        } catch (ApiException e) {
+            System.out.println("API error: " + e.getMessage());
+        }
+    }
+}
--- src/main/java/au/org/consumerdatastandards/conformance/PayloadValidator.java
+++ src/main/java/au/org/consumerdatastandards/conformance/PayloadValidator.java
@@ -2,35 +2,25 @@ package au.org.consumerdatastandards.conformance;
 
 import au.org.consumerdatastandards.codegen.ModelBuilder;
 import au.org.consumerdatastandards.codegen.generator.Options;
-import au.org.consumerdatastandards.codegen.util.ReflectionUtil;
 import au.org.consumerdatastandards.conformance.util.ConformanceUtil;
 import au.org.consumerdatastandards.conformance.util.ModelConformanceConverter;
-import au.org.consumerdatastandards.support.data.DataDefinition;
-import au.org.consumerdatastandards.support.data.Property;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PayloadValidator {
 
@@ -94,41 +84,4 @@ public class PayloadValidator {
         }
         return false;
     }
-    
-    public void checkAgainstModel(Object data, Class<?> model) throws IllegalAccessException {
-        LOGGER.info("Checking {} against {}", data, model);
-        List<Field> properties = ConformanceUtil.getAllProperties(model);
-        for (Field modelField : properties) {
-            Field dataField = FieldUtils.getField(data.getClass(), modelField.getName(), true);
-            assertNotNull(dataField, model.getSimpleName() + "." + modelField.getName() + " is missing from data " + data);
-            dataField.setAccessible(true);
-            Object dataFieldValue = dataField.get(data);
-            if (modelField.getAnnotation(Property.class).required()) {
-                assertNotNull(dataFieldValue, model.getSimpleName() + "." + modelField.getName() + " is required");
-            }
-            Class<?> modelFieldType = modelField.getType();
-            if (modelFieldType.isArray()) {
-                if (modelFieldType.getComponentType().isAnnotationPresent(DataDefinition.class)
-                    && dataFieldValue != null
-                    && Array.getLength(dataFieldValue) > 0) {
-                    Object[] values = ConformanceUtil.unpack(dataFieldValue);
-                    for (Object value : values) {
-                        checkAgainstModel(value, modelFieldType.getComponentType());
-                    }
-                }
-            } else if (ReflectionUtil.isSetOrList(modelFieldType)) {
-                Class<?> itemType = ReflectionUtil.getItemType(modelFieldType, modelField.getGenericType());
-                if (itemType.isAnnotationPresent(DataDefinition.class)
-                    && dataFieldValue != null && !((Collection) dataFieldValue).isEmpty()) {
-                    for (Object value : (Collection) dataFieldValue) {
-                        checkAgainstModel(value, itemType);
-                    }
-                }
-
-            } else if (dataFieldValue != null && modelFieldType.isAnnotationPresent(DataDefinition.class)) {
-                checkAgainstModel(dataFieldValue, modelFieldType);
-            }
-        }
-    }
-
 }
